bf9b1cf6f36d5aef77ddc1a8d52c5f984e58a533
Andy Yang <yang@cockroachlabs.com>
2025-03-11 12:39:47 -0400

    iterutil: add more iterator adapters
    Release note: None

--- pkg/util/iterutil/iterutil.go
+++ pkg/util/iterutil/iterutil.go
@@ -41,6 +41,34 @@ func Map(err error) error {
 	return err
 }
 
+// Concat takes an arbitrary number of iter.Seq's and concatenates them
+// into a single iter.Seq.
+func Concat[E any](seqs ...iter.Seq[E]) iter.Seq[E] {
+	return func(yield func(E) bool) {
+		for _, seq := range seqs {
+			for v := range seq {
+				if !yield(v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// Concat2 takes an arbitrary number of iter.Seq2's and concatenates them
+// into a single iter.Seq2.
+func Concat2[K, V any](seqs ...iter.Seq2[K, V]) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for _, seq := range seqs {
+			for k, v := range seq {
+				if !yield(k, v) {
+					return
+				}
+			}
+		}
+	}
+}
+
 // Enumerate takes an iter.Seq and returns an iter.Seq2 where the first value
 // is an int counter.
 func Enumerate[E any](seq iter.Seq[E]) iter.Seq2[int, E] {
@@ -55,12 +83,50 @@ func Enumerate[E any](seq iter.Seq[E]) iter.Seq2[int, E] {
 	}
 }
 
+// Filter2 takes an iter.Seq2 and returns a new iter.Seq2 that filters out any
+// elements that do not satisfy the provided filter function.
+func Filter2[K, V any](seq iter.Seq2[K, V], f func(K, V) bool) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k, v := range seq {
+			if f(k, v) {
+				if !yield(k, v) {
+					return
+				}
+			}
+		}
+	}
+}
+
+// MapSeq takes an iter.Seq and a map function and returns an iter.Seq2 where
+// the second value is the result of applying the map to the original value.
+func MapSeq[K, V any](seq iter.Seq[K], f func(K) V) iter.Seq2[K, V] {
+	return func(yield func(K, V) bool) {
+		for k := range seq {
+			if !yield(k, f(k)) {
+				return
+			}
+		}
+	}
+}
+
 // Keys takes an iter.Seq2 and returns an iter.Seq that iterates over the
 // first value in each pair of values.
 func Keys[K, V any](seq iter.Seq2[K, V]) iter.Seq[K] {
 	return func(yield func(K) bool) {
 		for k := range seq {
 			if !yield(k) {
+				return
+			}
+		}
+	}
+}
+
+// Values takes an iter.Seq2 and returns an iter.Seq that iterates over the
+// second value in each pair of values.
+func Values[K, V any](seq iter.Seq2[K, V]) iter.Seq[V] {
+	return func(yield func(V) bool) {
+		for _, v := range seq {
+			if !yield(v) {
 				return
 			}
 		}
--- pkg/util/iterutil/iterutil_test.go
+++ pkg/util/iterutil/iterutil_test.go
@@ -6,6 +6,7 @@
 package iterutil_test
 
 import (
+	"iter"
 	"maps"
 	"slices"
 	"testing"
@@ -13,6 +14,68 @@ import (
 	"github.com/cockroachdb/cockroach/pkg/util/iterutil"
 	"github.com/stretchr/testify/require"
 )
+
+func TestConcat(t *testing.T) {
+	for name, tc := range map[string]struct {
+		input [][]int
+	}{
+		"zero seqs": {
+			input: nil,
+		},
+		"one seq": {
+			input: [][]int{{1, 2, 3}},
+		},
+		"multiple seqs": {
+			input: [][]int{{1, 2, 3}, {4}, {5, 6}},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			var seqs []iter.Seq[int]
+			var expected []int
+			for _, s := range tc.input {
+				seqs = append(seqs, slices.Values(s))
+				expected = append(expected, s...)
+			}
+			actual := slices.Collect(iterutil.Concat(seqs...))
+			require.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestConcat2(t *testing.T) {
+	for name, tc := range map[string]struct {
+		input []map[int]int
+	}{
+		"zero seqs": {
+			input: []map[int]int{},
+		},
+		"one seq": {
+			input: []map[int]int{
+				{1: 1, 2: 2, 3: 3},
+			},
+		},
+		"multiple seqs": {
+			input: []map[int]int{
+				{1: 1, 2: 2, 3: 3},
+				{4: 4},
+				{5: 5, 6: 6},
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			var seqs []iter.Seq2[int, int]
+			expected := make(map[int]int)
+			for _, s := range tc.input {
+				seqs = append(seqs, maps.All(s))
+				for k, v := range s {
+					expected[k] = v
+				}
+			}
+			actual := maps.Collect(iterutil.Concat2(seqs...))
+			require.Equal(t, expected, actual)
+		})
+	}
+}
 
 func TestEnumerate(t *testing.T) {
 	for name, tc := range map[string]struct {
@@ -32,6 +95,40 @@ func TestEnumerate(t *testing.T) {
 			expected := maps.Collect(slices.All(tc.input))
 			actual := maps.Collect(iterutil.Enumerate(slices.Values(tc.input)))
 			require.Equal(t, expected, actual)
+		})
+	}
+}
+
+func TestFilter2(t *testing.T) {
+	// TODO(yang): Fill in.
+}
+
+func TestMapSeq(t *testing.T) {
+	for name, tc := range map[string]struct {
+		input []int
+		f     func(int) int
+	}{
+		"empty": {
+			input: nil,
+			f:     func(x int) int { return x + 1 },
+		},
+		"one element": {
+			input: []int{1},
+			f:     func(x int) int { return x + 1 },
+		},
+		"multiple elements": {
+			input: []int{1},
+			f:     func(x int) int { return x + 1 },
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			var expected []int
+			for _, x := range tc.input {
+				expected = append(expected, tc.f(x))
+			}
+			res := iterutil.MapSeq(slices.Values(tc.input), tc.f)
+			require.Equal(t, tc.input, slices.Collect(iterutil.Keys(res)))
+			require.Equal(t, expected, slices.Collect(iterutil.Values(res)))
 		})
 	}
 }
@@ -59,6 +156,34 @@ func TestKeys(t *testing.T) {
 		t.Run(name, func(t *testing.T) {
 			expected := slices.Collect(maps.Keys(tc.input))
 			actual := slices.Collect(iterutil.Keys(maps.All(tc.input)))
+			require.ElementsMatch(t, expected, actual)
+		})
+	}
+}
+
+func TestValues(t *testing.T) {
+	for name, tc := range map[string]struct {
+		input map[int]int
+	}{
+		"empty": {
+			input: nil,
+		},
+		"one element": {
+			input: map[int]int{
+				1: 1,
+			},
+		},
+		"multiple elements": {
+			input: map[int]int{
+				1: 1,
+				4: 2,
+				9: 3,
+			},
+		},
+	} {
+		t.Run(name, func(t *testing.T) {
+			expected := slices.Collect(maps.Values(tc.input))
+			actual := slices.Collect(iterutil.Values(maps.All(tc.input)))
 			require.ElementsMatch(t, expected, actual)
 		})
 	}
